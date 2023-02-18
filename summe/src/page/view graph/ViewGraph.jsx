20c2859079b242e9a50753cb5077e0a158e3809c
shunnnl <103299126+shunnnl@users.noreply.github.com>
2023-02-18 19:42:23 +0900

    d
    d

--- summe/src/page/view graph/ViewGraph.jsx
+++ summe/src/page/view graph/ViewGraph.jsx
@@ -1,35 +1,11 @@
 import { useState } from "react";
 import { useLocation } from "react-router";
-import axios from "axios";
-import { useEffect } from "react";
 import MostLanguage from "../../component/graph/MostLanguage";
 import Time from "../../component/graph/Time";
 
 export default function ViewGraph() {
-  const { state } = useLocation();
+  // const { state } = useLocation();
   console.log("View Graph props");
-  console.log(state);
-
-  //백엔드에 json형식으로 값을 전달: axios post
-  // const dataPost = async () => {
-  //   try {
-  //     setError(null);
-  //     setLoading(true);
-  //     const response = await axios.post(
-  //       "http://localhost:8000/link",
-  //       JSON.stringify(state)
-  //     );
-  //   } catch (e) {
-  //     setError(e);
-  //   }
-  //   setLoading(false);
-  // };
-  // if (loading) {
-  //   return <div>로딩중..</div>;
-  // }
-  // if (error) {
-  //   alert("오류가 발생하였습니다. 다시 시도하세요");
-  // }
 
   return (
     <>
