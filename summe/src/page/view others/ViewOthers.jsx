4e47adeea997708ca57c913198cc555ad1d54379
보경 <97583162+bokoo14@users.noreply.github.com>
2023-02-18 20:50:23 +0900

    feat: view others

--- summe/src/page/view others/ViewOthers.jsx
+++ summe/src/page/view others/ViewOthers.jsx
@@ -1,3 +1,12 @@
+import Box from "../../component/Box";
+
 export default function ViewOthers() {
-  return <h1>view others page</h1>;
+  return (
+    <>
+      <h1>view others page</h1>
+      <Box />
+      <Box />
+      <Box />
+    </>
+  );
 }
