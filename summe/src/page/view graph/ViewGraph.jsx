b4bc79d5355362dde7e390ec7b4cb572ea205da2
보경 <97583162+bokoo14@users.noreply.github.com>
2023-02-18 19:59:33 +0900

    style: button style

--- summe/src/page/view graph/ViewGraph.jsx
+++ summe/src/page/view graph/ViewGraph.jsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
-import { useLocation } from "react-router";
 import MostLanguage from "../../component/graph/MostLanguage";
 import Time from "../../component/graph/Time";
 import MonthlyCommit from "../../component/graph/MonthlyCommit";
 import TeamContribute from "../../component/graph/TeamContribute";
-
 
 export default function ViewGraph() {
   // const { state } = useLocation();
@@ -13,8 +10,8 @@ export default function ViewGraph() {
   return (
     <>
       <h1>view graph page</h1>
-      <Time/>
-      <MostLanguage/>
+      <Time />
+      <MostLanguage />
       <MonthlyCommit />
       <TeamContribute />
     </>
