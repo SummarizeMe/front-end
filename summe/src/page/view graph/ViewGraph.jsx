18ca99d8fa1420166f498331d9965ca045436a0c
Jiwon <87058411+raipen@users.noreply.github.com>
2023-04-01 00:26:30 +0900

    Fix: remove browser router and changed data passing method to useState from navigator #21

--- summe/src/page/view graph/ViewGraph.jsx
+++ summe/src/page/view graph/ViewGraph.jsx
@@ -5,10 +5,7 @@ import MonthlyCommit from "../../component/graph/MonthlyCommit";
 import TeamContribute from "../../component/graph/TeamContribute";
 import Blog from "../../component/graph/Blog";
 
-export default function ViewGraph() {
-  const { state } = useLocation();
-  //console.log("View Graph props", state);
-
+export default function ViewGraph({ state }) {
   return (
     <>
       <h1>view graph page</h1>
