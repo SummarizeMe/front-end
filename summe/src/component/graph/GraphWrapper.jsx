3eb8e7fc74ab276a7d427e6ad5f19f935537182a
보경 <97583162+bokoo14@users.noreply.github.com>
2023-04-01 18:16:13 +0900

    feat: loading #33

--- summe/src/component/graph/GraphWrapper.jsx
+++ summe/src/component/graph/GraphWrapper.jsx
@@ -1,26 +1,33 @@
 import ApexCharts from "react-apexcharts";
+import { Suspense } from "react";
+import "./GraphLoading.css";
 
+// 회전하는 로딩 중 표시
 const LoadingBox = () => {
-    return (
-        <div style={{ height: "100%", width: "100%" }}>
-            로딩중 ...
-        </div>
-    );
+  return <div className="loading-box"></div>;
 };
 
 export default function GraphWrapper({ data, type, height, width }) {
-    return (
-        <div style={{width:width,height:height,display:"inline-block"}}>
-            {data
-                ? <ApexCharts
-                    options={data.options}
-                    series={data.series}
-                    type={type}
-                    height="100%"
-                    width="100%"
-                />
-                : <LoadingBox/>
-            }
-        </div>
-    );
+  return (
+    <div
+      style={{
+        width: width,
+        height: height,
+        display: "inline-block",
+        position: "relative",
+      }}
+    >
+      {data ? (
+        <ApexCharts
+          options={data.options}
+          series={data.series}
+          type={type}
+          height="100%"
+          width="100%"
+        />
+      ) : (
+        <LoadingBox />
+      )}
+    </div>
+  );
 }
